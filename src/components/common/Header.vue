6d36da706dc4c1df03803d6f563f732c883b11f7
guntinue <rjssn93@gmail.com>
2026-01-19 12:34:40 +0900

    modify

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -88,7 +88,7 @@ const switchToRegister = () => {
       <input
           :class="$style.InputStyle"
           type="text"
-          placeholder="검색😎"
+          placeholder="😎검색😎"
           v-model="search"
           @keyup="onEnterkeyHandler"
           @input="onSearchChangeHandler"
